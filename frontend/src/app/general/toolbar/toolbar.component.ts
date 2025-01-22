2d11677da6293c7631e8c5b74bada39e0d739c02
FurKay00 <furkay68@outlook.com>
2025-01-22 22:53:34 +0100

    feat: toolbar component holds current week values and date

--- frontend/src/app/general/toolbar/toolbar.component.ts
+++ frontend/src/app/general/toolbar/toolbar.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatCheckbox} from '@angular/material/checkbox';
+import {startOfWeek, endOfWeek, eachDayOfInterval, addWeeks, subWeeks} from 'date-fns';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInput, MatInputModule, MatLabel} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,28 +17,63 @@ import {MatCheckbox} from '@angular/material/checkbox';
     MatIcon,
     MatIconButton,
     MatCheckbox,
-    MatButton
+    MatButton,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    DatePipe,
+    MatLabel,
+    MatInput
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+  @Output() weekDaysSelected  = new EventEmitter<Date[]>();
+
   showDistance = false;
   viewMode: 'day' | 'week' = 'week';
+  startOfWeek!: Date;
+  endOfWeek!: Date;
+  currentDate:Date = new Date();
+
+  constructor() {
+    this.resetToCurrentWeek();
+  }
 
   setViewMode(mode: 'day' | 'week'): void {
     this.viewMode = mode;
   }
 
-  goToday() {
-    console.log('Reset to today');
+  onDateSelected(selectedDate: Date): void {
+    this.startOfWeek = startOfWeek(selectedDate, { weekStartsOn: 1 }); // Week starts on Monday
+    this.endOfWeek = endOfWeek(selectedDate, { weekStartsOn: 1 });
+    this.currentDate = selectedDate;
+
+    this.emitWeekDays();
   }
 
-  previous() {
-    console.log('Go to previous');
+  setToNextWeek():void{
+    this.onDateSelected( addWeeks(this.currentDate, 1));
   }
 
-  next() {
-    console.log('Go to next');
+  setToLastWeek():void{
+    this.onDateSelected( subWeeks(this.currentDate, 1));
+  }
+
+  resetToCurrentWeek(): void {
+    const today = new Date();
+    this.currentDate = today;
+    this.startOfWeek = startOfWeek(today, { weekStartsOn: 1 });
+    this.endOfWeek = endOfWeek(today, { weekStartsOn: 1 });
+
+    this.emitWeekDays();
+  }
+
+  emitWeekDays(): void {
+    const weekDays = eachDayOfInterval({
+      start: this.startOfWeek,
+      end: this.endOfWeek,
+    });
+    this.weekDaysSelected.emit(weekDays);
   }
 }
